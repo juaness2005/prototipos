a53c9f1e6605110231a557b313de373de6d59f4d
Juan Gabriel Neves dos Santos <juangabriel4362767@gmail.com>
2026-03-22 22:14:33 -0300

    Add files via upload

--- henry.js
+++ henry.js
@@ -19,7 +19,7 @@ const mensagens = [
   "Vou te mostrar o paraíso se você for um anjo, só meu",
   "Por um tempo achei que era melhor estar seguro do que apaixonado, no entanto te conheci e a minha segurança falhou",
   "Tem uma placa em meu coração e ela diz que ele está reservado a você",
-  "Essa cidade é false, mas você é a coisa real",
+  "Essa cidade é falso, mas você é a coisa real",
   "Eu posso ser a vida que você tanto deseja",
   "Eu odeio isso daqui, então vou para um lugar em minha mente onde estamos juntos",
   "E se os meus olhos se voltassem para cima e encontrassem os seus mais uma vez?",
@@ -27,33 +27,39 @@ const mensagens = [
   "Não sou doador, mas te daria o meu coração caso você precisasse",
   "Garfos, facas, pratos, porcelanas, jogos americanos, luminárias, quando conheci você, tudo ficou mais bonito",
   "Tá tão fácil de encaixar os teus cénarios na minha rotina",
-  "Faz parte da minha rotina te querer"
+  "Faz parte da minha rotina te querer",
+  "Eu estarei com 87 e você com 89 e ainda sim eu olharei para você assim como olho para as estrelas que brilham no céu",
+  "Você me deixa louco na metade do tempo e na outra metade eu apenas estou tentando deixar você saber o que eu sinto é verdadeiro",
+  "Tudo que eu penso em como posso fazer você pensar em mim e em tudo o que poderiamos ser",
+  "Você é a unica coisa que me faz continuar fazendo pedidos para as estrelas",
+  "Isso é uma história de amor, apenas diga sim",
+  "O seu lugar é comigo, como não percebe isso",
+  "O seu sorriso poderia iluminar a cidade inteira",
+  "A sua voz é a melhor coisa que eu já ouvi",
+  "Eu gosto do fato que você é tudo o que eu sempre quis",
+  "Você é o pedido que eu peço as estrelas cadentes, para todos os horarios iguais, para quando o rélogio bate às 4:47, você é o que ue mais quero no mundo"
 ];
 
-// botão fechar popup
 document.getElementById("fechar").addEventListener("click", () => {
   popup.classList.add("hidden");
 
   setTimeout(() => {
     popup.style.display = "none";
-    popupFechado = true; // libera clique nos corações
+    popupFechado = true; 
   }, 300);
 });
 
-// cria corações baseado na quantidade de mensagens
 mensagens.forEach((texto) => {
   const heart = document.createElement("div");
   heart.classList.add("coracao");
   heart.textContent = "❤️";
 
-  // posição aleatória
   heart.style.top = (Math.random() * 90 + 5) + "%";
   heart.style.left = (Math.random() * 90 + 5) + "%";
 
 heart.addEventListener("click", () => {
   if (!popupFechado) return;
 
-  // evita contar duas vezes o mesmo coração
   if (heart.classList.contains("hidden")) return;
 
   if (lastClicked) {
@@ -65,10 +71,8 @@ heart.addEventListener("click", () => {
 
   lastClicked = heart;
 
-  // 🔥 conta cliques
   coracoesClicados++;
 
-  // 🎯 quando acabar todos
   if (coracoesClicados === mensagens.length) {
     setTimeout(() => {
       document.getElementById("popupFinal").classList.remove("hidden");
